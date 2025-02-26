a93547cf7a18f3d74c498c33478f9b60b9914585
khushal <khushalsharma40312@gmail.com>
2025-02-26 21:52:28 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -33,7 +33,7 @@ const authLink = setContext((_, { headers }) => {
 // WebSocket Link for subscriptions
 const wsLink = new GraphQLWsLink(
   createClient({
-    url: 'ws://https://socialmedia-realtime-backend.onrender.com/graphql',
+    url: 'ws://socialmedia-realtime-backend.onrender.com/graphql',
     connectionParams: () => {
       const token = localStorage.getItem("token");
       return { token: token || '' };
