31c7a0e8017033105a14d66ed4e2b8d9769f5f28
khushal <khushalsharma40312@gmail.com>
2025-02-28 13:38:43 +0530

    Update Chat.jsx

--- src/routes/Chat.jsx
+++ src/routes/Chat.jsx
@@ -271,12 +271,12 @@ const Chat = () => {
       <div className={`${theme} mobile-width`} style={{ width: "66vw", backgroundColor: "#1e1e1e", color: "#fff", padding: "20px" }}>
         <div className="d-flex">
           <img
-            src={user.picture || "/default.jpg"}
+            src={user?.picture || "/default.jpg"}
             alt="profile"
             style={{ width: "45px", height: "45px", marginBottom: "7px", borderRadius: "50%", cursor: "pointer" }}
             onClick={(e) => {
               e.preventDefault(); // Prevent navigation
-              setZoomImage(user.picture || "/default.jpg");
+              setZoomImage(user?.picture || "/default.jpg");
             }}
           />
 
