7da99f4cf47dffe2194dc91ff8b154987f03f8c5
khushal <khushalsharma40312@gmail.com>
2025-03-01 17:11:10 +0530

    Update VisitedProfileHeader.jsx

--- src/components/VisitedProfileHeader.jsx
+++ src/components/VisitedProfileHeader.jsx
@@ -22,7 +22,8 @@ const isfollower= followerslist.some(f=>f.id===localStorage.getItem("userId"))
                 <h4 className={` ${theme} my-3`}>Profile</h4>
             </div>
             <div className={` ${theme} d-flex justify-content-center`} style={{ height: "90vh", width: "95vw" }}>
-                <div className={`hrmargin ${theme}`} style={{ width: "43vw", borderRadius: "20px", border: '1px solid rgb(47 46 46)' }}>
+                               <div className={`hrmargin ${theme} profile-mob`} style={{ width: "43vw", borderRadius: "20px", border: '1px solid rgb(47 46 46)' }}>
+
                     <div className='my-3 d-flex justify-content-between'>
                         <h2 className='my-4 mx-3'>{userInfo.firstName || "Username"}</h2>
                         {/* Profile Picture */}
