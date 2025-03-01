9dc46edba20b6dab0aaa2f916dfd747fb717f7bc
khushal <khushalsharma40312@gmail.com>
2025-03-01 16:50:20 +0530

    Update LoadingSpinner.jsx

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 export default function LoadingSpinner() {
   return (
     <>
-       <div className="d-flex my-5 justify-content-center spinner">
-            <div
-                className="spinner-border"
-                role="status"
-                style={{ width: "5rem", height: "5rem" }}
-            >
-                <span className="visually-hidden">Loading...</span>
-            </div>
+      <div style={{ marginBottom: "100px" }} className="d-flex  justify-content-center spinner">
+        <div
+          className="spinner-border"
+          role="status"
+          style={{ width: "5rem", height: "5rem" }}
+        >
+          <span className="visually-hidden">Loading...</span>
         </div>
+      </div>
     </>
   )
 }
